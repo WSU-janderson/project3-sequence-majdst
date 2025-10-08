9f39283852f839929a9b66fbbb6f8b0769b79a95
Majid Dousti <dousti.explore@gmail.com>
2025-10-08 00:23:27 -0400

    Sequence class-private part

--- Sequence.h
+++ Sequence.h
@@ -16,3 +16,23 @@ public: //made it public to give access
     SequenceNode(const std::string& val):item(val){}
 
 };
+
+//next class--> sequence class
+class Sequence {
+private:
+    // first node pointer
+    SequenceNode* head_ = nullptr;
+    //last node pointer
+    SequenceNode* tail_ = nullptr;
+    // initialize the number of elements
+    std::size_t size = 0;
+
+    /*
+     *This is a helper function to prevent to repeat the for loop everytime we are
+     *looking for node
+     */
+    SequenceNode* nodeAt(std:: size_t idx) const;
+
+public:
+    
+};
