87939248f104241b5de890ec5fd77e94b77d0fde
Majid Dousti <dousti.explore@gmail.com>
2025-10-08 10:37:28 -0400

    progressing in header file-->not easy :(

--- Sequence.h
+++ Sequence.h
@@ -34,5 +34,42 @@ private:
     SequenceNode* nodeAt(std:: size_t idx) const;
 
 public:
+    //Rule of Three--> ensure object created, copied and destroyed
+    //no memory leak
+    explicit Sequence(std::size_t size=0); //create
+
+    Sequence(const Sequence& other); //copy
+
+    ~Sequence(); //delete
+
+    Sequence& operator = (const Sequence& rhs); //right hand side copy
+
+    // giving access to element in linked list
+
+    std::string& operator[](std::size_t position); //access to item(index)
+
+    std::string front() const; //checking sequence-->if front empty-->exception
+
+    std::string back() const; //checking sequence-->if back empty-->exception
+
+    //Adding & Removing
+
+    void push_back(std::string item); //append to sequence
+
+    void pop_back(); //delete last item
+
+    /*
+     * the main difference between linked list and array--> shift right
+     */
+
+    void insert(std::size_t position, std::string item); //insert to position
+
+    void erase(std::size_t position); //remove items at position
+
+    void erase(std::size_t position, std::size_t count); //remove the count item
+
     
+
+
+
 };
