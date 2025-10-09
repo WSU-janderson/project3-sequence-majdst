3ed53a550da4bab596d57d617aef92c78ec68154
Majid Dousti <dousti.explore@gmail.com>
2025-10-09 15:42:13 -0400

    Continue working on cpp file

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,7 +1,7 @@
 #include "Sequence.h"
 #include <stdexcept>
 #include <ostream>
-#include <algorithm>
+#include <iostream>
 
 using std::size_t;
 using std::string;
@@ -13,5 +13,36 @@ using std:: out_of_range;
 SequenceNode* Sequence::nodeAt(size_t idx) const {
     if (idx >= size_) {
         throw out_of_range("index out of range");
+    }
+
+    SequenceNode* current = head_; //start at the head
+
+    //walking forward
+    for (size_t i = 0; i < idx; i++) {
+
+        current = current ->next;
+    }
+    return current;
+}
+
+// Creates a sequence of sz items
+Sequence::Sequence(size_t sz):head_(nullptr), tail_(nullptr), size_(0) {
+
+    for (size_t i = 0; i < sz; i++){push_back("");} //empty string-->new element
+}
+
+//memory cleaning
+Sequence::~Sequence(){clear();}
+//create a deep copy-->
+/*
+ * remember: this part create a deep copy, if I just copy the head/tail--> they will
+ * point to the same memory as others-->if other delete, list will break.
+ */
+Sequence::Sequence(const Sequence& other) : head_(0), tail_(0), size_(0) {
+
+    SequenceNode* current = other.head_;
+    while (current != nullptr) {
+        push_back(current->item);
+        current = current->next;
     }
 }
